007d58258fdeb07e1f0e22528b3a33a8a59db1de
Matthew <mrentz@mail.com>
2017-11-06 20:40:14 +1030

    working comments form with nested attributes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+
+  def create
+    book = Book.find params[:book_id]
+    comment = book.comments.new post_params
+    comment.save
+    flash[:notice] = "Comment successfully saved"
+    redirect_to book_path(book)
+  end
+
+  private
+  def post_params
+    params.require(:comment).permit(:author, :text)
+  end
+  
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  has_many :comments
+  accepts_nested_attributes_for :comments
 end
