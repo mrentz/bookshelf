48f6c58af3115d22ec772f9b0cd65cd06f6d8a74
Matthew <mrentz@mail.com>
2017-11-12 04:56:57 +1030

    admion adds new books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,28 @@ class BooksController < ApplicationController
     @books = Book.paginate :page => params[:page], :per_page => 10
   end
 
-  def show
-    @book = Book.find(params[:id])
-    @comments = @book.comments
+  def new
+    @book = Book.new
   end
 
+  def create
+    @book = Book.new post_params
+    if @book.save
+      flash[:notice] = "#{@book.title} successfully saved"
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+    @comments = @book.comments.paginate :page => params[:page], :per_page => 10 
+  end
+
+  private
+  def post_params
+    params.require(:book).permit(:title, :thoughts)
+  end
+  
 end
