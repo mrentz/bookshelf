5c916fef8981e508327995885e2cb5424b558f7e
Matthew <mrentz@mail.com>
2017-11-12 07:45:19 +1030

    successfully added ordering of the book list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
 
   def index
 #    @users = User.paginate(page: params[:page])
-    @books = Book.paginate :page => params[:page], :per_page => 10
+    @books = Book.paginate(:page => params[:page], :per_page => 10).order('created_at DESC')
   end
 
   def new
